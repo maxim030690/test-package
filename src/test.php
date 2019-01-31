9a7f0773b0a260a03b2d57edbcff12e609200cad
Max.Tkachov <maxim030690@gmail.com>
2019-01-31 22:25:20 +0200

    #First commit

---
+++ src/test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace tkachev\images;
+
+use tkachev\images\Images;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ImagesTest
+ */
+class ImagesTest extends TestCase{
+
+
+    /**
+     * @var string
+     */
+    private $url = 'https://kor.ill.in.ua/m/610x386/2271658.jpg';
+
+
+    /**
+     * Test on true
+     */
+    function testTrue() {
+        $images = new Images();
+        $this->assertTrue($images->uploadImage($this->url));
+    }
+}
+
